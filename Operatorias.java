6bb99cf6f408b8a13bcaafe4dc9bea6ba1dcc5d1
zh4r0nax <30630616+zh4r0nax@users.noreply.github.com>
2018-05-30 14:28:39 -0400

    class Operatorias

---
+++ Operatorias.java
@@ -0,0 +1,22 @@
+public class Operatorias{
+    public static void main(String args[]){
+        int numero_uno = 4;
+        int numero_dos = 4;
+        int res = 0;
+
+        res = numero_uno + numero_dos;
+        System.out.println("El resultado es:: "+res);
+
+        res = numero_uno - numero_dos;
+        System.out.println("El resultado es:: "+res);
+
+        res = numero_uno * numero_dos;
+        System.out.println("El resultado es:: "+res);
+
+        res = numero_uno / numero_dos;
+        System.out.println("El resultado es:: "+res);
+
+
+
+    }
+}
