d830f17df73afcfcbe19e9a7b21296b27ce6d6fb
zh4r0nax <30630616+zh4r0nax@users.noreply.github.com>
2018-05-30 22:25:45 -0400

    Operadores

---
+++ Operadores.java
@@ -0,0 +1,12 @@
+public class Operadores{
+	public static void main(String args[]){
+		int numero_uno = 2;
+		char numero_dos = '2';
+        
+        if(numero_dos == numero_uno){
+            System.out.println("es igual");
+        }else{
+            System.out.println("NO es igual");
+        }
+	}
+}
